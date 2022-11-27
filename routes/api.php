e921cbc784a7bb5cd2f018781a23cb82a77ab0ca
Alexandre Salgueiro <erdleosalgueiro@gmail.com>
2022-11-27 00:32:53 -0300

    Teste de api com ThunderClient

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('/teste',function (Request $request) {
+    return $request->all();
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
